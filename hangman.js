9036b55be994948302a84d548faa657471cf3512
Charles Thomas Bowden <charles.bowden@gmail.com>
2017-12-14 16:39:29 -0500

    end of day save

--- hangman.js
+++ hangman.js
@@ -11,3 +11,24 @@ var inquirer = require("inquirer");
 // Imported objects
 var word = require("./word.js");
 var letter = require("./letter.js");
+
+var lives = 5;
+
+// While Loop to repeat Inquirer prompts? Until Victory or Defeat
+while (lives > 0){
+	inquirer.prompt([
+	{
+		name: "letter",
+		message: "Welcome to Hangman! Choose a letter."
+	}
+	]).then(function(answers){
+		var someLetter = new Letter(answers.letter);
+	})
+
+
+if (lives > 0) {
+	console.log("Congrats You're a Winner!");
+} else {
+	console.log("Sorry Try Again?");
+};
+
--- letter.js
+++ letter.js
@@ -3,6 +3,9 @@
 var Letter = function(Letter) {
 	// Property to store the letter input
 	this.letter = letter;
+	this.printInfo = function() {
+		console.log(this.letter);
+	}
 };
 
 module.exports = ;
